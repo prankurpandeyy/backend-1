d8f9da8e7c64b41f3149126390d4a51adbe033ab
prankurpandeyy <pprankur@gmail.com>
2025-01-02 21:08:37 +0530

    changed routes

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,9 +1,9 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import accountRoutes from './routes/account.routes';
-import loginRoutes from './routes/login.routes';
-import swaggerUi from 'swagger-ui-express';
-import { specs } from './config/swagger';
+import express from "express";
+import dotenv from "dotenv";
+import accountRoutes from "../routes/account.routes";
+import loginRoutes from "../routes/login.routes";
+import swaggerUi from "swagger-ui-express";
+import { specs } from "../config/swagger";
 
 dotenv.config();
 
@@ -15,22 +15,31 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Routes
-app.use('/api/account', accountRoutes);
-app.use('/api/auth', loginRoutes);
+app.use("/api/account", accountRoutes);
+app.use("/api/auth", loginRoutes);
 
 // Swagger documentation route
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
 
 // Error handling middleware
-app.use((err: any, req: express.Request, res: express.Response, next: express.NextFunction) => {
-  console.error(err.stack);
-  res.status(500).json({
-    success: false,
-    message: 'Internal server error',
-    error: err.message
-  });
-});
+app.use(
+  (
+    err: any,
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+  ) => {
+    console.error(err.stack);
+    res.status(500).json({
+      success: false,
+      message: "Internal server error",
+      error: err.message,
+    });
+  }
+);
+
+app.get("/", (req, res) => res.send("Express on Vercel"));
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
-}); 
+});
